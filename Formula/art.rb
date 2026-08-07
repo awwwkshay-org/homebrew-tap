fc540bc9db089d098af1b554273d6dbbc88422ec
Akshay Priyadarshi <akshayp1904@outlook.com>
2026-08-07 23:01:50 +0530

    Update art to v0.2.0

--- Formula/art.rb
+++ Formula/art.rb
@@ -1,26 +1,26 @@
 class Art < Formula
   desc "Generate production-oriented Rust full-stack monorepos"
   homepage "https://github.com/awwwkshay-org/awesome-rust-templates"
-  version "0.1.0"
+  version "0.2.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.1.0/art-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "974306982a56a642f348b90e2c17d430aecc9f43e30754ca10adb589b140aa92"
+      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.2.0/art-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "53e40b1d8d5d09c84483333cbfb69404be2bd8fc84dd8de2b9c132d41577d008"
     else
-      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.1.0/art-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "eff2f8157c3e546d35fb1bb4ffaef1d4a89d2b67c16cca0e75ca56ab42315af2"
+      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.2.0/art-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "299fff68f10cd6dff671054f54ffb1b1cf01a7b6ae4e871bbedd2f54a9ec6635"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.1.0/art-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "852121a605b23058b1505c8f15454253ca652a493a9b452c4b4111fe76f69d38"
+      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.2.0/art-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "eec31416f6a5b3f1ea6cc8f42ab84b305b871f47bca7b897e9785e890a3875f3"
     else
-      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.1.0/art-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ca7c9c739d4237cc0c9fcbe93c74346cbca3c96b4064fc73f303417119a3f944"
+      url "https://github.com/awwwkshay-org/awesome-rust-templates/releases/download/v0.2.0/art-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ad49713eb4c7ae124c59d1756d1af28de641a4606f3ad92e04ac165bd1a1b468"
     end
   end
 
